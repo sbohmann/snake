aa9751359d8d96b4d1477845f43a0ba60ebc38b3
sbohmann <github@yeoman.at>
2024-09-21 09:13:48 +0200

    Space, Escape

--- snake.js
+++ snake.js
@@ -1,28 +1,51 @@
 window.onload = setup
 
+const None = Object.freeze({keyCode: 'Space', name: 'None', move: moveFunction(0, 0)})
 const Up = Object.freeze({keyCode: 'ArrowUp', name: 'Up', move: moveFunction(0, -1)})
 const Down = Object.freeze({keyCode: 'ArrowDown', name: 'Down', move: moveFunction(0, 1)})
 const Left = Object.freeze({keyCode: 'ArrowLeft', name: 'Left', move: moveFunction(-1, 0)})
 const Right = Object.freeze({keyCode: 'ArrowRight', name: 'Right', move: moveFunction(1, 0)})
-const Directions = Object.freeze([Up, Down, Left, Right])
+const Directions = Object.freeze([None, Up, Down, Left, Right])
 
 function setup() {
-    const directionQueue = []
+    const gridSize = 20;
     const snakeBoard = document.getElementById('snake-board')
-    const canvas = document.getElementById("snake-board");
-    snakeBoard.style.background = 'yellow'
     const gc = snakeBoard.getContext('2d')
-    let x= 0
-    let y = 0
-    let currentDirection = Right
+
+    let x
+    let y
+    let currentDirection
+    let directionQueue
+    let lastDirection
+
+    setInitialState()
+
+    function setInitialState() {
+        x = 0
+        y = 0
+        currentDirection = None
+        directionQueue = []
+    }
+
     window.onkeydown = event => {
+        if (event.code === 'Escape') {
+            setInitialState()
+            gc.reset()
+            gc.clearRect(0, 0, snakeBoard.width, snakeBoard.height)
+            draw()
+            return
+        }
         for (let direction of Directions) {
             if (event.code === direction.keyCode) {
                 directionQueue.push(direction)
             }
         }
     }
+
+    draw()
+
     const interval = setInterval(move, 100)
+
     function move() {
         while (directionQueue.length > 0) {
             let newDirection = directionQueue.shift()
@@ -32,20 +55,39 @@ function setup() {
             }
         }
         [x, y] = currentDirection.move(x, y)
-        gc.fillStyle = 'black'
-        gc.fillRect(x * 10, y * 10, 10, 10)
+        draw();
     }
+
+    function draw() {
+        gc.fillStyle = 'black'
+        gc.fillRect(x * gridSize, y * gridSize, gridSize, gridSize)
+    }
+
     function directionApplicable(direction) {
+        if (direction === None && currentDirection !== None) {
+            lastDirection = currentDirection
+            return true
+        }
+
         switch (currentDirection) {
+            case None:
+                return direction !== opposite(lastDirection)
             case Up:
             case Down:
                 return direction === Left || direction === Right
             case Left:
             case Right:
                 return direction === Up || direction === Down
-            default:
-                throw RangeError()
         }
+    }
+}
+
+function opposite(direction) {
+    switch (direction) {
+        case Up: return Down
+        case Down: return Up
+        case Left: return Right
+        case Right: return Left
     }
 }
 
